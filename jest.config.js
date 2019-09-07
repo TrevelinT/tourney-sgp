8163dcc0de142f2a477ba4caa215dea62db87ac7
Danilo Trevelin <danilo.muller.trevelin@gmail.com>
2019-09-06 23:53:25 -0300

    chore(tests): update config

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
-  testEnvironment: "node",
+  modulePaths: ["<rootDir>/app"],
+  moduleFileExtensions: ["ts", "tsx", "js", "json", "jsx", "node"],
   setupFilesAfterEnv: ["<rootDir>internals/testing/setup.js"],
   snapshotSerializers: ["jest-emotion"],
 };
